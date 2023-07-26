b7eb2953535bc66f0275eb340d3dc52dca52eb3b
Bikesh Shrestha <bikesh@sharelookapp.com>
2023-07-26 18:30:27 +0545

    changes

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
       >
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
-            <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-6/12">
+            <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-6/12 sm:pb-16">
               {/* <div className="mb-12 max-w-[360px] lg:mb-16">
                 <Link href="/" className="mb-8 inline-block">
                   <Image
