846d4dcd5d05b28dd8be2eb932da2776a6084267
Bikesh Shrestha <bikesh@sharelookapp.com>
2023-07-27 09:35:25 +0545

    changes

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -46,7 +46,7 @@ const Header = () => {
       >
         <div className="container">
           <div className="relative -mx-4 flex items-center justify-between">
-            <div className="w-60 max-w-full px-4 xl:mr-12">
+            <div className="w-80 max-w-full px-4 xl:mr-12">
               <Link
                 href="/"
                 className={`header-logo block w-full ${
